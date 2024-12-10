0fea1a61a4dfc1d53c4d21e90745053a66ba6222
Muhammad_Sami <160221069+muhammadsami987123@users.noreply.github.com>
2024-12-10 15:05:13 +0500

    update

---
+++ src/app/mageGrid.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+export default function ImageGrid(){ 
+  return (
+    <div className="bg-white py-9 ">
+      <h2 className="text-xl  font-semibold text-center ">Share your setup with</h2> 
+      <h2 className="text-5xl font-extrabold text-center ">#FuniroFurniture</h2>
+      <div className="md:grid-cols-2 lg:grid-cols-3 py-4 gap-4">
+        <Image src={"/grid.jpg"} className="w-full h-[721px] object-cover" alt={''} />
+      </div>
+    </div>
+  );
+}
